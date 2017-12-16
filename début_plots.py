2328f3cd536f1422a4ac88f80e061e9cc1c0b2f2
EnguerrandPrebet <enguerrand.prebet@ens-lyon.fr>
2017-12-16 15:26:11 +0100

    Add seed as folder
    - image for res = 0.25, 20 tests, T = 10'000

--- début_plots.py
+++ début_plots.py
@@ -5,7 +5,8 @@ from fractions import *
 from sys import *
 from random import *
 import subprocess as sp
-import os.path
+import os
+import errno
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
@@ -14,10 +15,7 @@ import seaborn as sns
 sns.set()
 #sp.run("make", shell=True)
 
-my_seed = 42
-seed(my_seed)
-
-K = 10 # number of tests
+K = 20 # number of tests
 
 
 def avancement(_,K):
@@ -25,14 +23,14 @@ def avancement(_,K):
     print("|"+"#"*int(30*_/K)+" "*(30-int(30*_/K))+"|",_, "out of", K, "\t\t", end='\r')
 
 
-def ex1_get(alpha,beta):
+def ex1_get(alpha,beta,seed): #seed as string
     # Parameters
     N=1000
     M=int(alpha*N)
     T=10000
     
     # File name
-    filename = "ex1_" + str(alpha) + "_" + str(beta) + ".tmp"
+    filename = seed+"/ex1_" + str(alpha) + "_" + str(beta) + ".tmp"
     
     # Have confirmation of overwriting
     if os.path.isfile(filename):
@@ -58,32 +56,32 @@ def ex1_get(alpha,beta):
             e_t = float(file.readline().split()[0]) # e_t is the 1st value
             avg_energy[t] += e_t/K
  
-def ex1_plot(alpha,beta):
+def ex1_plot(seed,alpha,beta):
     # Parameters
     T=10000
     
     X = [i for i in range(T+1)]
-    Y = ex1_get(alpha,beta)
+    Y = ex1_get(alpha,beta,seed)
     
     fig = plt.figure()
     plt.plot(X,Y)
     plt.xlabel('time')
     plt.ylabel('energy')
     plt.title('Energy evolution, alpha=%f, beta=%f' % (alpha,beta))
-    fig.savefig('ex1.png')
+    fig.savefig('ex1-'+seed+'.png')
     
     
     
     
     
-def ex2_get(alpha,beta):
+def ex2_get(alpha,beta,seed): #seed as string
     # Parameters
     N = 100
     M = alpha*N
-    T = 1000
+    T = 10000
     
     # File name
-    filename = "ex2_" + str(alpha) + "_" + str(beta) + ".tmp"
+    filename = seed+"/ex2_" + str(alpha) + "_" + str(beta) + ".tmp"
     
     # Have confirmation of overwriting
     #if os.path.isfile(filename):
@@ -108,7 +106,7 @@ def ex2_get(alpha,beta):
 
     return avg_energy
         
-def ex2_plot(res=0.5):
+def ex2_plot(seed,res=0.5):
     # Parameters
     a_min = 0.5
     a_max = 5
@@ -120,22 +118,22 @@ def ex2_plot(res=0.5):
     
     for i in range(l):
         for j in range(l):
-            heatmap_matrix[j][i] = ex2_get( a_range[i], a_range[j])
+            heatmap_matrix[j][i] = ex2_get( a_range[i], a_range[j],seed)
     
-    heatmap = sns.heatmap(heatmap_matrix, annot=True, cmap="YlGnBu", vmin=0, vmax=0.3)
+    heatmap = sns.heatmap(heatmap_matrix, cmap="YlGnBu", vmin=0, vmax=0.3)
     #heatmap.invert_yaxis()
     heatmap.set_title('Energy map depending on alpha,beta')
     heatmap.set_xlabel('alpha')
     heatmap.set_ylabel('beta')
     heatmap.set_xticklabels(a_range)
     heatmap.set_yticklabels(a_range)
-    heatmap.figure.savefig('ex2.png')
+    heatmap.figure.savefig('ex2-'+seed+'.png')
     
     
     
     
     
-def ex3_get(alpha,beta):
+def ex3_get(alpha,beta,seed): #seed as string
     # Parameters
     K=250 # number of tests
     N=1000
@@ -143,7 +141,7 @@ def ex3_get(alpha,beta):
     T=10000
     
     # File name
-    filename = "ex3_" + str(alpha) + "_" + str(beta) + ".tmp"
+    filename = seed+"/ex3_" + str(alpha) + "_" + str(beta) + ".tmp"
     
     # Have confirmation of overwriting
     if os.path.isfile(filename):
@@ -169,7 +167,7 @@ def ex3_get(alpha,beta):
 
     return avg_q
     
-def ex3_plot(res=0.5):
+def ex3_plot(seed,res=0.5):
     # Parameters
     a_min = 0.5
     a_max = 5
@@ -181,11 +179,29 @@ def ex3_plot(res=0.5):
     
     for i in range(l):
         for j in range(l):
-            heatmap_matrix[i][j] = ex2_get( a_range[i], a_range[j])
+            heatmap_matrix[i][j] = ex2_get( a_range[i], a_range[j],seed)
     
     heatmap = sns.heatmap(heatmap_matrix, cmap="YlGnBu")
     heatmap.set_title('Overlap map depending on alpha,beta')
     heatmap.set_xlabel('alpha')
     heatmap.set_ylabel('beta')
-    heatmap.savefig('ex3.png')
+    heatmap.savefig('ex3-'+seed+'.png')
     
+	
+def setseed(s=None): 
+    if(s == None):
+        seed()
+        s = randint(0,maxsize)
+    seed(s)
+    s = str(s)
+    try:
+        os.makedirs(s)
+    except OSError as exception:
+        if exception.errno != errno.EEXIST:
+            raise
+    return str(s)
+	
+def delete(directory):
+    for file in os.listdir(directory):
+        os.remove(os.path.join(directory,file))
+    os.rmdir(directory)
